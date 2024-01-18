e73ae36b950c02cdb9661aab32b74e762529c2a8
Rogok-Serg <rogoksv@gmail.com>
2024-01-18 09:50:58 +0200

    create project test

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+// add(1, 1);
+let total = 100;
+let userName = "bob";
+let isActive = false;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+// add(1, 1);
+
+let total: number = 100;
+let userName: string = "bob";
+let isActive: boolean = false;
